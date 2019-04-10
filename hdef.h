f9a79ba8655b746a0b06c509848c31bfe0598976
ithewei <ithewei@163.com>
2019-04-10 20:20:33 +0800

    update

--- hdef.h
+++ hdef.h
@@ -3,15 +3,16 @@
 
 #include "hplatform.h"
 
+#include <stdint.h>
 typedef unsigned char       uint8;
 typedef unsigned short      uint16;
 typedef unsigned int        uint32;
-typedef unsigned long long  uint64;
+typedef uint64_t            uint64;
 
 typedef char                int8;
 typedef short               int16;
 typedef int                 int32;
-typedef long long           int64;
+typedef int64_t             int64;
 
 typedef float               float32;
 typedef double              float64;
