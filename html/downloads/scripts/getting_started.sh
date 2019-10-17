3f041fad65d24aa72ac3230ae6763ce0cabbad7a
ithewei <ithewei@163.com>
2019-10-17 19:56:45 +0800

    update

---
+++ html/downloads/scripts/getting_started.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make httpd curl
+
+bin/httpd -d
+ps aux | grep httpd
+
+# http web service
+bin/curl -v localhost:8080
+
+# indexof
+bin/curl -v localhost:8080/downloads/
+
+# http api service
+bin/curl -v -X POST localhost:8086/v1/api/json -H "Content-Type:application/json" -d '{"user":"admin","pswd":"123456"}'
