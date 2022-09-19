158cff2fcef3f22ba24fd16338c060c7fccef523
MrSen <53619621+NorthCityChen@users.noreply.github.com>
2022-09-19 15:02:35 +0800

    set SOCK_CLOEXEC (#268)
    Co-authored-by: 管培喆 <guanpeizhe@minieye.cc>

--- base/hsocket.c
+++ base/hsocket.c
@@ -266,7 +266,11 @@ int Bind(int port, const char* host, int type) {
 }
 
 int Listen(int port, const char* host) {
+#ifdef OS_LINUX
+    int sockfd = Bind(port, host, SOCK_STREAM|SOCK_CLOEXEC);
+#else
     int sockfd = Bind(port, host, SOCK_STREAM);
+#endif
     if (sockfd < 0) return sockfd;
     return ListenFD(sockfd);
 }
