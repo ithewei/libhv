a56fc78a96b0fc76181dc4ca3c7f6da845c84b04
Yang Bin <1420506535@qq.com>
2024-05-31 21:43:24 +0800

    fix: nullptr caused by failed HttpHandler::SwitchHTTP2()(#560). (#561)

--- http/server/HttpHandler.cpp
+++ http/server/HttpHandler.cpp
@@ -153,10 +153,11 @@ void HttpHandler::Close() {
 }
 
 bool HttpHandler::SwitchHTTP2() {
-    parser.reset(HttpParser::New(HTTP_SERVER, ::HTTP_V2));
-    if (parser == NULL) {
+    HttpParser* http2_parser = HttpParser::New(HTTP_SERVER, ::HTTP_V2);
+    if (http2_parser == NULL) {
         return false;
     }
+    parser.reset(http2_parser);
     protocol = HTTP_V2;
     resp->http_major = req->http_major = 2;
     resp->http_minor = req->http_minor = 0;
